265df88c708865b8431be0a54015e219cfebe170
FireGuo1145 <fireguo1145@flweb.cn>
2026-09-02 22:05:17 +0800

    chore: Delete old code

--- internal/channel/sqlite_migration_models.go
+++
@@ -1,7 +0,0 @@
-package channel
-
-import "github.com/veloce-ailab/veloce/internal/model"
-
-func init() {
-	model.RegisterSQLiteMigrationModels(&MessageChannelIntegration{}, &MessageChannelMessage{})
-}
